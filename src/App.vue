1ed234564cb584932a293dfad9dbe1784f0e161c
Pichu Chen <pichu@tih.tw>
2022-02-24 09:08:10 +0800

    update

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <!-- <img alt="Vue logo" src="./assets/logo.png"> -->
+  <FirstPage msg="Welcome to Your Vue.js App"/>
+</template>
+
+<script>
+import FirstPage from './components/FirstPage.vue'
+
+export default {
+  name: 'App',
+  components: {
+    FirstPage
+  }
+}
+</script>
+
+<style>
+#appxx {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
